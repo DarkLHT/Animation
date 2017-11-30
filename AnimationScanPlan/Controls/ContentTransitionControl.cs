4f60b813d31e610ddb6317bfb00be30b8763e917
hamza.hamid <hamza.hamid@olympus-ossa.com>
2017-11-30 11:54:52 -0500

    First Commit

---
+++ AnimationScanPlan/Controls/ContentTransitionControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace AnimationScanPlan.Controls
+{
+    public class ContentTransitionControl : ContentControl
+    {
+        public static readonly DependencyProperty DurationProperty = DependencyProperty.Register(
+            "Duration",
+            typeof(Duration),
+            typeof(ContentTransitionControl));
+
+        public Duration Duration
+        {
+            get { return (Duration)GetValue(DurationProperty); }
+            set { SetValue(DurationProperty, value); }
+        }
+
+        public double CurrentTop { get; set; }
+
+        private ContentControl _current;
+        private ContentControl _old;
+        private double _oldTop;
+
+
+        public ContentTransitionControl()
+        {
+            DefaultStyleKey = typeof(ContentTransitionControl);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            _old = (ContentControl)GetTemplateChild("_old");
+            _current = (ContentControl)GetTemplateChild("_current");
+            base.OnApplyTemplate();
+        }
+
+        protected override void OnContentChanged(object oldContent, object newContent)
+        {
+            base.OnContentChanged(oldContent, newContent);
+
+            if (_current == null || _old == null) return;
+
+            _current.Content = newContent;
+            _old.Content = oldContent;
+
+            var width = ActualWidth;
+
+            Slide(new Thickness(0, _oldTop, 0, 0), new Thickness(width, _oldTop, 0, 0), _old);
+            Slide(new Thickness(-width, CurrentTop, width, 0), new Thickness(0, CurrentTop, 0, 0), _current);
+
+            _oldTop = CurrentTop;
+        }
+
+        private void Slide(Thickness from, Thickness to, DependencyObject element)
+        {
+            if (element == null) return;
+
+            var slide = new ThicknessAnimation
+            {
+                Duration = Duration,
+                From = from,
+                To = to,
+                DecelerationRatio = 0.9
+            };
+
+            Storyboard.SetTarget(slide, element);
+            Storyboard.SetTargetProperty(slide, new PropertyPath("Margin"));
+
+            var sb = new Storyboard();
+            sb.Children.Add(slide);
+            sb.Begin();
+        }
+    }
+}
---
+++ AnimationScanPlan/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+
+namespace AnimationScanPlan.ViewModels
+{
+    public enum SomeView
+    {
+        View1,
+        View2
+    }
+
+    public class MainWindowViewModel : BindableBase
+    {
+        private SomeView _someView;
+        private DelegateCommand _view1Command;
+        private DelegateCommand _view2Command;
+
+        public SomeView SomeView
+        {
+            get { return _someView; }
+            set { _someView = value; OnPropertyChanged(); }
+        }
+
+        public DelegateCommand View1Command => _view1Command ?? (_view1Command = new DelegateCommand(View1Action));
+        public DelegateCommand View2Command => _view2Command ?? (_view2Command = new DelegateCommand(View2Action));
+
+        public MainWindowViewModel()
+        {
+            SomeView = SomeView.View1;
+        }
+
+        private void View2Action()
+        {
+            SomeView = SomeView.View2;
+        }
+
+        private void View1Action()
+        {
+            SomeView = SomeView.View1;
+        }
+    }
+}
